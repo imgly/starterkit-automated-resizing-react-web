5f4d6cc668384231c2eb7c1816aa76dde29a908b
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-26 10:23:43 +0200

    Update starterkit for v1.81.1

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -17,7 +17,7 @@ import type { Template } from '../imgly/types';
  */
 export const DEMO_ASSETS_BASE_URL: string =
   import.meta.env.VITE_DEMO_ASSETS_BASE_URL ||
-  'https://staticimgly.com/imgly/cesdk-web-examples-data/1.81.0/starterkit-automated-resizing';
+  'https://staticimgly.com/imgly/cesdk-web-examples-data/1.81.1/starterkit-automated-resizing';
 
 // ============================================================================
 // Size Presets
